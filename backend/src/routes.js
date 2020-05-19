044c7adb6106d2a2fd96fe321afb5263aedbd1de
Thanatoss2408 <guilhermer30@gmail.com>
2020-05-19 19:35:24 -0300

    up

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,11 @@ const  express = require('express')
 const productsController = require('./controllers/productsController')
 const routes= express.Router()
 
-routes.get('products/', productsController.index)
-routes.get('product/:id',productsController.show)
-routes.post('products/',productsController.create)
-routes.put('products/:id',productsController.update)
-routes.delete('products/:id',productsController.destroy)
+routes.get('/products/', productsController.index)
+routes.get('/product/:id',productsController.show)
+routes.post('/products/',productsController.create)
+routes.put('/product/:id',productsController.update)
+routes.delete('/product/:id',productsController.destroy)
 
 
 
